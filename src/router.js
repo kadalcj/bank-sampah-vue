9d5b26d556a37a41abb04efb40727eff628cd0cc
Theo <adryantheo@gmail.com>
2020-08-04 15:19:38 +0700

    Fixing Routing

--- src/router.js
+++ src/router.js
@@ -4,9 +4,9 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/', component: () => import('./components/Root.vue'/* webpackChunkName: "js/chunk-root-admin" */)},
+    { path: '/', component: () => import('./components/Root.vue'/* webpackChunkName: "js/chunk-root-admin" */), meta: {requiresAuth: true},},
     { path: '/login', component: () => import('./components/login/login.vue' /* webpackChunkName: "js/chunk-login-page" */)},
-    { path : '/dashboard', component: () => import('./components/base/Dashboard.vue' /* webpackChunkName: "js/chunk-dashboard-page" */)},
+    { path : '/dashboard', component: () => import('./components/base/Dashboard.vue' /* webpackChunkName: "js/chunk-dashboard-page" */), meta: {requiresAuth: true},},
     { path : '/register', component: () => import('./components/login/register.vue' /* webpackChunkName: "js/chunk-register-page" */)},
 ]
 
@@ -15,5 +15,22 @@ const router = new VueRouter({
     hashbang: false,
     mode: 'history',
 });
+
+
+router.beforeEach(async(to,from,next) => {
+    const userId = localStorage.getItem('userid');
+    if(to.matched.some(route => route.meta.requiresAuth)){
+        if(!userId){
+            next({path: '/login', replace:true})
+            true
+        }
+    }else{
+        if(userId){
+            next({path: '/dashboard', replace:true})
+            return
+        }
+    }
+    next();
+})
 
 export default router;
